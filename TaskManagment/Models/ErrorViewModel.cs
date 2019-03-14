928925e62e5a8301f2c9ad65c3a4978fdf0982c2
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-03-14 19:06:08 +0200

    Add project files.

---
+++ TaskManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
