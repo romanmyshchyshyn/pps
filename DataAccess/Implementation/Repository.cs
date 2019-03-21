6f43e9c5073da56d10bd6f6ebc9ab9ace1fd8291
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-03-21 21:42:45 +0200

    Add patterns repository and unitofwork

---
+++ DataAccess/Implementation/Repository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Implementation
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        protected readonly TmDbContext Context;
+
+        public Repository(TmDbContext context)
+        {
+            Context = context;
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void Add(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void Remove(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Set<TEntity>().Update(entity);
+        }
+    }
+}
---
+++ DataAccess/Implementation/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TmDbContext _context;
+
+        private Dictionary<Type, object> _repositories;
+
+        public UnitOfWork(TmDbContext context)
+        {
+            _context = context;
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>()
+            where TEntity : class
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Dictionary<Type, object>();
+            }
+
+            Type type = typeof(TEntity);
+            if (!_repositories.ContainsKey(type))
+            {
+                _repositories[type] = new Repository<TEntity>(_context);
+            }
+
+            return (IRepository<TEntity>)_repositories[type];
+        }
+    }
+}
