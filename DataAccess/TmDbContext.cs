239c454c44b13bd8347d28b2b321357413c604fb
MellowOrange <>
2019-04-04 16:33:15 +0300

    ensure database creating

--- DataAccess/TmDbContext.cs
+++ DataAccess/TmDbContext.cs
@@ -17,6 +17,7 @@ namespace DataAccess
         public TmDbContext(DbContextOptions<TmDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
