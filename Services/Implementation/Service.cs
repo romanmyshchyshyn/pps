500ee6f7b5cc743f7e1f053d8ba90ae0bd14b3aa
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-04-12 05:14:23 +0300

    add base class, interface, filter

---
+++ Services/Implementation/Service.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Services.Implementation
+{
+    public abstract class Service<TEntity, TDto, TFilter> : IService<TDto, TFilter>
+    where TEntity : class
+    where TDto : class
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+
+        protected Service(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        protected abstract TDto MapToDto(TEntity entity);
+        protected abstract TEntity MapToEntity(TDto dto);
+
+        protected IRepository<TEntity> Repository => _unitOfWork.GetRepository<TEntity>();
+
+        public abstract TDto Get(int id);
+        public abstract IEnumerable<TDto> Get(TFilter filter);
+        public abstract void Add(TDto dto);
+        public abstract void Remove(int id);
+        public abstract void Update(TDto dto);
+    }
+}
---
+++ Services/Interfaces/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Interfaces
+{
+    public interface IService<TDto, TFilter>
+    {
+        TDto Get(int id);
+        IEnumerable<TDto> Get(TFilter filter);
+        void Add(TDto dto);
+        void Remove(int id);
+        void Update(TDto dto);
+    }
+}
