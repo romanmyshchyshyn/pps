a3456b7f4e9a2860e4d0b052884f60b2da088f91
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-05-25 16:58:06 +0300

    sign in form

--- TaskManagment/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TaskManagment/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -38,15 +38,12 @@ namespace TaskManagment.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [Display(Name = "Login")]
-            public string UserName { get; set; }
+            [Display(Name = "Email")]
+            public string Email { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
-
-            [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -74,7 +71,7 @@ namespace TaskManagment.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -82,7 +79,7 @@ namespace TaskManagment.Areas.Identity.Pages.Account
                 }
                 if (result.RequiresTwoFactor)
                 {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = false });
                 }
                 if (result.IsLockedOut)
                 {
